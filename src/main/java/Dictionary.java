353916c0d3e3a6e820d3f1db431bc3e6cc805b93
arlen <burton.arlen.1990@gmail.com>
2016-05-01 15:45:52 -0700

    added dictionary scripts and web pages

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -18,6 +18,67 @@ public class Dictionary {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    
+    get("/words/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/AddWords.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/words/:id/definition/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Words lexicon = Words.locate(Integer.parseInt(request.params(":id")));
+      model.put("lexicon", lexicon);
+      model.put("template", "templates/CreateDefine.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/words", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("words", Words.all());
+      model.put("template", "templates/word.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/definitions", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("definitions", Definition.all());
+      model.put("template", "templates/define.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/words", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Words newWords = new Words(name);
+      model.put("template", "templates/define.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/definitionPost", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Words lexicon = Words.locate(Integer.parseInt(request.queryParams("lexiconId")));
+      String description = request.queryParams("description");
+      Definition definition = new Definition(description);
+      lexicon.addDefine(definition);
+      model.put("lexicon", lexicon);
+      model.put("template", "templates/AddWords.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/words/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Words lexicon = Words.locate(Integer.parseInt(request.params(":id")));
+      model.put("lexicon", lexicon);
+      model.put("template", "templates/word.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/definitions/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Definition definition = Definition.locate(Integer.parseInt(request.params(":id")));
+      model.put("definition", definition);
+      model.put("template", "templates/definition.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
