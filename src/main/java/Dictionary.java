89f69358b4313280a7b14bc537add06d86f94d41
arlen <burton.arlen.1990@gmail.com>
2016-05-02 00:00:22 -0700

    fixed layout issue

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -6,7 +6,7 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 import java.util.ArrayList;
 
-public class App {
+public class Dictionary {
   public static void main(String[] args) {
 
     // String layout = "templates/layout.vtl";
